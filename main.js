464b13783261bbc0f798aa82fcce3f41612a2fc0
liliilililiilll <artistdiary331@gmail.com>
2025-01-07 11:53:24 +0900

    Update main.js.sugar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
+var client_tid = "mYt/UPi9OAGIERr2l78fZuEhiprXrHAA7tlad2cyQ1gd2aIWF7t7//2cUu0kZ9/hw6K2tJrI/NxevcBrwq8Zkm8hemeCmg
+"; // replace by X-Client-Transaction-Id value
 var client_uuid = "***"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
@@ -8,7 +10,7 @@ var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "liliilililiilll" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
